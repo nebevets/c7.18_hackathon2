e70650155bafb155ceee0714860753a939db66bd
dangrt5 <randy.dang05@gmail.com>
2018-09-06 16:16:34 -0700

    added responsive sizing for small screens

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ function createCluesOnDom(clueObj){
   let newOl = $('<ol>');
 
 	let newFileForm = $('<div>', {
-		'class': 'form-group'
+		'class': 'form-group col-md-12'
 	});
 	let newLabel = $('<label>', {
 		'class': 'control-label col-xs-12',
@@ -156,13 +156,13 @@ function createCluesOnDom(clueObj){
 	});
 	let newSkipButton = $('<button>', {
 		'type': 'button',
-		'class': 'skip btn btn-default col-xs-12 col-sm-2 col-sm-push-1 col-md-4 col-md-push-1',
+		'class': 'skip btn btn-default col-xs-12 col-sm-4 col-sm-push-1 col-md-4 col-md-push-1',
 		'text': 'Skip Clue',
 		'click': () => skipButtonHandler()
 	});
 	let newLeaderBoardButton = $('<button>', {
 		'type': 'button',
-		'class': 'leaderBoard btn btn-info col-xs-12 col-sm-2 col-sm-push-1 col-md-4 col-md-push-2',
+		'class': 'leaderBoard btn btn-info col-xs-12 col-sm-4 col-sm-push-2 col-md-4 col-md-push-2',
 		'text': 'Leader Board',
 		'click': () => leaderboardButtonHandler()
 	});
@@ -631,7 +631,7 @@ function addPlayerToGame(playerName){
 	if(totalPlayersObj[playerName]){
 		player.score = totalPlayersObj[playerName].score;
 	}
-	else{ 
+	else{
 		totalPlayersObj[playerName] = {score: 0};
 	}
 	saveGameData();
