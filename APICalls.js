4e996eb676b6732a69b6d44f3bc8c41eced0923a
dangrt5 <randy.dang05@gmail.com>
2018-10-24 13:55:50 -0700

    added error handlign to flickr ajax function

--- APICalls.js
+++ APICalls.js
@@ -6,26 +6,34 @@
 function getRandomImageFromFlickr(wordArray){
 	getQuote();
 	let searchKeyWordList = wordArray;
-  	let randomKeyWord = searchKeyWordList[getRandomInt(0, searchKeyWordList.length-1)];
+	let randomKeyWord = searchKeyWordList[getRandomInt(0, searchKeyWordList.length-1)];
+	let flickrAttempts = 0;
+
+	if(flickrAttempts > 3) {
+		waitingModal("Error: Server is not responding. Please refresh the game and try again.")
+		// errorModal("Error: Server is not responding. Please refresh the game and try again.")
+	}
+
 	const flickrConfig = {
 		url: `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${flickrAPIKey}&format=json&nojsoncallback=1&text=${randomKeyWord}&per_page=5`,
     	success: result => {
-      		const randomPhoto = result.photos.photo[getRandomInt(0, result.photos.photo.length-1)]
-      		if(randomPhoto === undefined) {
-				randomKeyWord = searchKeyWordList[getRandomInt(0, searchKeyWordList.length-1)];
-				flickrConfig.url = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${flickrAPIKey}&format=json&nojsoncallback=1&text=${randomKeyWord}&per_page=5`;
-				$.ajax(flickrConfig);
-      		}
-    		let flickrImgURL = `https://farm${randomPhoto.farm}.staticflickr.com/${randomPhoto.server}/${randomPhoto.id}_${randomPhoto.secret}.jpg`
-			savedGameImages.clueImg = flickrImgURL;
-    		clarifai.models.predict(Clarifai.GENERAL_MODEL, flickrImgURL).then(response => {
-        		let clarifaiResponse = response;
-        		clueImg = clarifaiResponse.outputs[0].data.concepts;
-        		createCluesOnDom(clueImg);
-    		});
+	  		const randomPhoto = result.photos.photo[getRandomInt(0, result.photos.photo.length-1)]
+				if(randomPhoto === undefined && flickrAttempts < 3) {
+					randomKeyWord = searchKeyWordList[getRandomInt(0, searchKeyWordList.length-1)];
+					flickrAttempts++;
+					flickrConfig.url = `https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${flickrAPIKey}&format=json&nojsoncallback=1&text=${randomKeyWord}&per_page=5`;
+					$.ajax(flickrConfig);
+	  		}
+	  		let flickrImgURL = 	`https://farm${randomPhoto.farm}.staticflickr.com/${randomPhoto.server}/${randomPhoto.id}_${randomPhoto.secret}.jpg`;
+				savedGameImages.clueImg = flickrImgURL;
+	  		clarifai.models.predict(Clarifai.GENERAL_MODEL, flickrImgURL).then(response => {
+	    		let clarifaiResponse = response;
+	    		clueImg = clarifaiResponse.outputs[0].data.concepts;
+	    		createCluesOnDom(clueImg);
+				});
     	}
   	}
-  	$.ajax(flickrConfig);
+	$.ajax(flickrConfig);
 }
 /****************************************************************************************************
  * description: gets a quote from geek jokes
