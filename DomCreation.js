bc5f3d05040659e77ca4bba556dc1a1674d6379a
nebevets <steveben@exmsft.com>
2018-10-25 10:01:44 -0700

    removed dom creation of eyespy logo and added static image to page.

--- DomCreation.js
+++ DomCreation.js
@@ -6,10 +6,6 @@
  * @return:none
  */
 function instructionsPage(){
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	});
 	let newLandingPageContainer = $('<div>', {
 		'class': 'landingPage form-group'
 	});
@@ -62,7 +58,7 @@ function instructionsPage(){
 	newPlayerForm.append(newLabel, newInput);
 	newButtonForm.append(newGoBtn, newLeaderBoardButton);
 	newLandingPageContainer.append(newInstructions, newPlayerForm, newButtonForm);
-	$('.container').append(eyeSpyLogo, newLandingPageContainer);
+	$('.container').append(newLandingPageContainer);
 }
 /****************************************************************************************************
 * description: creates the clue page
@@ -70,10 +66,6 @@ function instructionsPage(){
  * @return: none
  */
 function createCluesOnDom(clueObj){
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	});
 	let newCluesContainer = $('<div>', {
 		class: 'cluesPage form-group'
 	});
@@ -132,7 +124,7 @@ function createCluesOnDom(clueObj){
 	newButtonForm.append(newSkipButton, newLeaderBoardButton);
 	newClues.append(newH4, newOl);
 	newCluesContainer.append(newInstructions, newClues, newFileForm, newButtonForm);
-	$('.container').append(eyeSpyLogo, newCluesContainer);
+	$('.container').append(newCluesContainer);
 	let imageLoader = $('#uploadFile');
 	imageLoader.change(handleImage);
 }
@@ -142,10 +134,6 @@ function createCluesOnDom(clueObj){
  * @return: none
  */
 function getResultsPage(){
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	});
 	let newResultsPage = $('<div>', {
 		'class': 'resultsPage'
 	});
@@ -224,7 +212,7 @@ function getResultsPage(){
 	buttonCol.append(getClueButton, newLeaderBoardButton);
 	secondRow.append(buttonCol);
 	newResultsPage.append(firstRow, updateUser, secondRow);
-	$('.container').append(eyeSpyLogo, newResultsPage);
+	$('.container').append(newResultsPage);
 }
 /****************************************************************************************************
 * description: sets up leader board page with dom creation
@@ -232,10 +220,6 @@ function getResultsPage(){
  * @return:
  */
 function getLeaderBoardPage(){
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	});
 	let newLeaderBoardPage = $('<div>', {
 		'class': 'leaderBoard'
 	});
@@ -278,7 +262,7 @@ function getLeaderBoardPage(){
 	newLeaderBoardPage.append(firstRow);
 	addPlayersToLeaderBoard(totalPlayersObj, newLeaderBoardPage);
 	newLeaderBoardPage.append(buttonRow);
-	$('.container').append(eyeSpyLogo, newLeaderBoardPage)
+	$('.container').append(newLeaderBoardPage)
 }
 /****************************************************************************************************
  * description: this adds players to the leader board, calls saveGameData().
@@ -334,17 +318,13 @@ function addPlayersToLeaderBoard(playerObjFromFirebase, htmlElement){
  */
 function waitingModal(quote){
 	$('.modal-body').empty();
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	})
 	$('.modal-title').text(`Hello ${player.name}, some entertainment while you're waiting`);
 	let quoteOfTheDay = $('<div>', {
 		class: 'quoteDiv',
 		text: quote,
 	})
 
-	$('.modal-body').append(eyeSpyLogo, quoteOfTheDay);
+	$('.modal-body').append(quoteOfTheDay);
 	$('#waitingModal').modal('show');
 }
 /*****************************************************************************************************/
@@ -356,17 +336,13 @@ function waitingModal(quote){
  */
 function errorModal(message){
 	$('.modal-body').empty();
-	let eyeSpyLogo = $('<img>', {
-		class: 'logoImg',
-		src: 'assets/eyespylogo.png',
-	})
 	$('.modal-title').text(`Error`);
 	let errorMessage = $('<div>', {
 		class: 'quoteDiv',
 		text: message,
 	})
 
-	$('.modal-body').append(eyeSpyLogo, errorMessage);
+	$('.modal-body').append(errorMessage);
 	$('#waitingModal').modal('show');
 }
 /*****************************************************************************************************/
